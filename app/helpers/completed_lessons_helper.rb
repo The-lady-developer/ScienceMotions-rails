64ec117a134db9076fd9dee9ff6b9603d9e7c742
Jessica Leach <jessica@info.nl>
2018-09-19 17:26:25 +0200

    puts and methods

--- app/helpers/completed_lessons_helper.rb
+++ app/helpers/completed_lessons_helper.rb
@@ -1,10 +1,26 @@
 module CompletedLessonsHelper
     # I know the method name sucks a little
-    def show_if_completed_lesson_id_is_31(completed_lesson = @completed_lesson)
-      if completed_lesson.lesson_id==31
-        "Hello"
+    def show_if_completed_lesson_id_is_completed(completed_lesson = @completed_lesson)
+      if completed_lesson.completed==true
+        "completed"
       else
-        "Anonymous"
+        "not completed"
       end
     end
+
+    def is_lesson_already_in_completed_lessons(lessons = @lessons, completed_lessons = @completed_lessons)
+        five = 5.even?()
+        # lessons.compare?(completed_lessons) #true
+        puts @lessons #true
+        puts "hai"
+        puts five
+
+        # if lesson.id==completed_lesson.id
+        #   "completed"
+        # else
+        #   "not completed"
+        # end
+    end
+
+
 end
