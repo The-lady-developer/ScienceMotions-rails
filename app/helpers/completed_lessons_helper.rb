483d44eb60a973b62cd3aa1ffa77f10f5484c045
Jessica Leach <jessica@info.nl>
2018-09-19 15:09:10 +0200

    if else statement works

--- app/helpers/completed_lessons_helper.rb
+++ app/helpers/completed_lessons_helper.rb
@@ -1,2 +1,10 @@
 module CompletedLessonsHelper
+    # I know the method name sucks a little
+    def show_if_completed_lesson_id_is_31(completed_lesson = @completed_lesson)
+      if completed_lesson.lesson_id==31
+        "Hello"
+      else
+        "Anonymous"
+      end
+    end
 end
