34f0568fc3ec77d7f95ff882894b0892add3766e
Jessica Leach <jessica@info.nl>
2018-09-19 17:26:31 +0200

    puts and methods

--- app/helpers/completed_lessons_helper.rb
+++ app/helpers/completed_lessons_helper.rb
@@ -1,20 +1,38 @@
 module CompletedLessonsHelper
     # I know the method name sucks a little
-    def show_if_completed_lesson_id_is_completed(completed_lesson = @completed_lesson)
-      if completed_lesson.completed==true
-        "completed"
-      else
-        "not completed"
-      end
-    end
+    # def show_if_completed_lesson_id_is_completed(completed_lesson = @completed_lesson)
+    #   if completed_lesson.completed==true
+    #     "completed"
+    #   else
+    #     "not completed"
+    # end
+    # end
 
-    def is_lesson_already_in_completed_lessons(lessons = @lessons, completed_lessons = @completed_lessons)
+    def is_lesson_already_in_completed_lessons(lesson = @lesson, completed_lesson = @completed_lesson)
         five = 5.even?()
         # lessons.compare?(completed_lessons) #true
-        puts @lessons #true
-        puts "hai"
-        puts five
 
+        lesson_id = lesson.id
+        completed_lesson_id = completed_lesson.lesson_id
+        completed_lesson_completed = completed_lesson.completed
+
+        puts lesson_id
+        puts completed_lesson_id
+        puts completed_lesson_completed
+
+        puts "bum face!!"
+
+        if lesson_id=completed_lesson_id
+            "lesson is completed"
+          else
+            "lesson not completed"
+        end
+
+        # if @completed_lesson.lesson_id==31
+        #     puts "completed_lesson 31"
+        #   else
+        #     puts "completed_lesson not 31"
+        # end
         # if lesson.id==completed_lesson.id
         #   "completed"
         # else
